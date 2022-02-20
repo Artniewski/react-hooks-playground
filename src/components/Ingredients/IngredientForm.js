4d8e1f51f6baa918d2204d15982c2f58021e4f7c
artniewski <artniewski@gmail.com>
2022-02-20 19:54:05 +0100

    Add useState Hook

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,33 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
-    event.preventDefault();
-    // ...
-  };
+    const [currentTitle, setCurrentTitle] = useState('');
+    const [currentAmount, setCurrentAmount] = useState('');
 
-  return (
-    <section className="ingredient-form">
-      <Card>
-        <form onSubmit={submitHandler}>
-          <div className="form-control">
-            <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
-          </div>
-          <div className="ingredient-form__actions">
-            <button type="submit">Add Ingredient</button>
-          </div>
-        </form>
-      </Card>
-    </section>
-  );
+    const submitHandler = event => {
+        event.preventDefault();
+        props.onAddIngredient({title: currentTitle, amount: currentAmount});
+    };
+
+    return (
+        <section className="ingredient-form">
+            <Card>
+                <form onSubmit={submitHandler}>
+                    <div className="form-control">
+                        <label htmlFor="title">Name</label>
+                        <input type="text" id="title" value={currentTitle}
+                               onChange={event => setCurrentTitle(event.target.value)}/>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="amount">Amount</label>
+                        <input type="number" id="amount" value={currentAmount}
+                               onChange={event => setCurrentAmount(event.target.value)}/>
+                    </div>
+                    <div className="ingredient-form__actions">
+                        <button type="submit">Add Ingredient</button>
+                    </div>
+                </form>
+            </Card>
+        </section>
+    );
 });
 
 export default IngredientForm;
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from "./IngredientList";
 
 function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
+    const [userIngredients, setUserIngredients] = useState([]);
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
-  );
+    const addIngredientHandler = ingredient => {
+        setUserIngredients(prevIngredients => [
+            ...prevIngredients,
+            {id: Math.random().toString(), ...ingredient}
+        ]);
+    };
+
+    const removeIngredientHandler = ingredientId => {
+        setUserIngredients(prevIngredients => {
+            return prevIngredients.filter(ingredient => ingredient.id !== ingredientId);
+        })
+    }
+    return (
+        <div className="App">
+            <IngredientForm onAddIngredient={addIngredientHandler}/>
+
+            <section>
+                <Search/>
+                <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
+            </section>
+        </div>
+    );
 }
 
 export default Ingredients;
